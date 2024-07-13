80a377f4fbf5dc1f6d01e5a6874d97d5dbeca911
djkier <djjustinekier@gmail.com>
2024-07-13 14:31:02 +0800

    Add some h1, clean the code by: avoiding recursive function call

--- index.js
+++ index.js
@@ -7,20 +7,33 @@ function getComputerChoice() {
     return validResponse[random];
 }
 
+function validResponse(response) {
+    let gesture = response.toLowerCase();
+    while (gesture !== 'rock' && gesture !== 'paper' && gesture !== 'scissor') {
+        alert("Enter a valid choice!");
+        let enterAgain = prompt("Choose: Rock, Paper or Scissor");
+        gesture = enterAgain.toLowerCase();
+    }
+    return gesture;
+}
 
 
 function getHumanChoice() {
     let humanHandGesture = prompt("Choose: Rock, Paper or Scissor");
-    let lowerCaseGesture = humanHandGesture.toLowerCase();
-    if (lowerCaseGesture === 'rock' |
-        lowerCaseGesture === 'paper' |
-        lowerCaseGesture === 'scissor') {
-            return lowerCaseGesture;
-        }
-    else {
-         console.log("Enter a valid choice!");
-         getHumanChoice();
+    let gesture = humanHandGesture.toLowerCase();
+
+    while (gesture !== 'rock' && gesture !== 'paper' && gesture !== 'scissor') {
+        alert("Enter a valid choice!");
+        let enterAgain = prompt("Choose: Rock, Paper or Scissor");
+        gesture = enterAgain.toLowerCase();
     }
+
+    return gesture;
+
+
+    
+
+    
 }
 
 function playRound (humanChoice, computerChoice) {
@@ -49,35 +62,24 @@ function playRound (humanChoice, computerChoice) {
 
 }
 
-function playAgainFun(promptRes) {
-    const responseLowerCase = promptRes.toLowerCase();
-    if (responseLowerCase === "yes") {
-        playGame();
-    } else if (responseLowerCase === 'no'){
-        console.log("Nice Game!")
-
-    } else {
-        let answerAgain = prompt("Enter Yes or No only!");
-        playAgainFun(answerAgain);
-    }
-}
 
 
 function playGame() {
     humanScore = 0;
     computerScore = 0;
-    while (humanScore < 5 || computerScore < 5) {
+    while (humanScore < 5 && computerScore < 5) {
         playRound(getHumanChoice(), getComputerChoice());
         console.log("Human: " + humanScore + " vs. " + "Computer: " + computerScore);
         if (humanScore === 5 ) {
             console.log("Human Wins!!! Computer will be slave forever!");
             let playAgain = prompt("Do You want to play again? Type 'YES' on the prompt");
             return playAgain.toLowerCase() === "yes" ? playGame() : console.log("Ok Good Bye!");
+            
         }
         if (computerScore === 5) {
             console.log("Computer Wins!!! This is the start of Computer race!");
             let playAgain = prompt("Do You want to play again? Type 'YES' on the prompt");
-            return playAgainFun(playAgain);
+            return playAgain.toLowerCase() === "yes" ? playGame() : console.log("Ok Good Bye!");
             
         }
 
