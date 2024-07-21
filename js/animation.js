e913596583e9896f9a1ea63909841a51fa286722
djkier <djjustinekier@gmail.com>
2024-07-21 21:04:07 +0800

    added animations through css and eventlistener in js

--- js/animation.js
+++ js/animation.js
@@ -4,24 +4,23 @@ const primaryHover = (event) => {
     const targetText = event.target.textContent;
     const imgChanger= document.querySelector("#img-container img");
 
-
-
     document.querySelector("#img-container h1").style.display = "none";
     imgChanger.style.display = "";
 
     imgChanger.src = `../resources/img/${targetText.toLowerCase()}.svg`;
     imgChanger.alt = targetText.toLowerCase();
+    imgChanger.style.animation = "shaking 0.7s ease-in-out infinite";
 
     document.querySelector("#img-container").style.boxShadow = "10px 10px 5px 0px rgba(2,2,2,1)"
 
 
-
-    
-
+   
 };
+
 
 const primaryHoverOut = (event) => {
     document.querySelector("#img-container").style.boxShadow = "";
+    document.querySelector("#img-container img").style.animation = '';
 }
 
 export { primaryHover, primaryHoverOut };
