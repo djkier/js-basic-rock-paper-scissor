7ad76532a87188b5d3bc5615ea5b14adacdefa71
djkier <djjustinekier@gmail.com>
2024-07-21 20:36:02 +0800

    change rock, paper, scissor svg files, Added hover styles on headings, img, and play game button , hover on headings will change the img, added events for hovering

--- js/animation.js
+++ js/animation.js
@@ -1,2 +1,27 @@
 //primary animation
 
+const primaryHover = (event) => {
+    const targetText = event.target.textContent;
+    const imgChanger= document.querySelector("#img-container img");
+
+
+
+    document.querySelector("#img-container h1").style.display = "none";
+    imgChanger.style.display = "";
+
+    imgChanger.src = `../resources/img/${targetText.toLowerCase()}.svg`;
+    imgChanger.alt = targetText.toLowerCase();
+
+    document.querySelector("#img-container").style.boxShadow = "10px 10px 5px 0px rgba(2,2,2,1)"
+
+
+
+    
+
+};
+
+const primaryHoverOut = (event) => {
+    document.querySelector("#img-container").style.boxShadow = "";
+}
+
+export { primaryHover, primaryHoverOut };
--- js/event.js
+++ js/event.js
@@ -1,3 +1,10 @@
 //primary
+import { primaryHover, primaryHoverOut } from './animation.js';
+
 
 const frontHeading = document.querySelector("#front-heading");
+
+frontHeading.addEventListener('mouseover', (e) => primaryHover(e));
+frontHeading.addEventListener('mouseout', (e) => primaryHoverOut(e));
+
+
