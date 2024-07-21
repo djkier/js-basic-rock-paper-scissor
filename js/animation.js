9c4735e093f98fea988645349fa85db921c48283
djkier <djjustinekier@gmail.com>
2024-07-21 23:13:12 +0800

    testing h1 node if it will give the right animation for the h1 tags on the primary page, added some responsiveness,

--- js/animation.js
+++ js/animation.js
@@ -1,26 +1,47 @@
 //primary animation
+const imgContainer = document.querySelector("#img-container");
+const image = document.querySelector("#img-container img");
+const h1Children = document.querySelectorAll("#front-heading h1");
 
 const primaryHover = (event) => {
     const targetText = event.target.textContent;
-    const imgChanger= document.querySelector("#img-container img");
 
     document.querySelector("#img-container h1").style.display = "none";
-    imgChanger.style.display = "";
 
-    imgChanger.src = `../resources/img/${targetText.toLowerCase()}.svg`;
-    imgChanger.alt = targetText.toLowerCase();
-    imgChanger.style.animation = "shaking 0.7s ease-in-out infinite";
+    image.style.display = "";
+    image.src = `../resources/img/${targetText.toLowerCase()}.svg`;
+    image.alt = targetText.toLowerCase();
+    image.style.animation = "shaking 0.7s ease-in-out infinite";
 
-    document.querySelector("#img-container").style.boxShadow = "10px 10px 5px 0px rgba(2,2,2,1)"
+    imgContainer.style.boxShadow = "10px 10px 5px 0px rgba(2,2,2,1)"
 
-
-   
 };
 
-
 const primaryHoverOut = (event) => {
-    document.querySelector("#img-container").style.boxShadow = "";
-    document.querySelector("#img-container img").style.animation = '';
-}
+    imgContainer.style.boxShadow = "";
+    image.style.animation = '';
+};
 
-export { primaryHover, primaryHoverOut };
+const startGameAnimation = () => { 
+    console.log(h1Children.length);
+
+    for (let i = 0; i < h1Children.length; i++) {
+        h1Children[i].style.animation = `xTranslate ${0.8 + (i * 0.6)}s ease-out`;
+        h1Children[i].addEventListener("animationend", () => {
+            h1Children[i].style.visibility = "hidden";
+        });
+    }
+
+
+    // h1Children.forEach(item => {
+        
+    //     item.style.animation = "xTranslate 1.2s ease-out";
+    //     item.addEventListener("animationend", () => { h1FirstChild.style.visibility = "hidden";
+    //     });
+    // });
+
+    
+    
+};
+
+export { primaryHover, primaryHoverOut, startGameAnimation };
--- js/event.js
+++ js/event.js
@@ -1,10 +1,12 @@
 //primary
-import { primaryHover, primaryHoverOut } from './animation.js';
+import { primaryHover, primaryHoverOut, startGameAnimation } from './animation.js';
 
 
 const frontHeading = document.querySelector("#front-heading");
+const startButton = document.querySelector("#primary button");
 
 frontHeading.addEventListener('mouseover', (e) => primaryHover(e));
 frontHeading.addEventListener('mouseout', (e) => primaryHoverOut(e));
+startButton.addEventListener('click', startGameAnimation);
 
 
